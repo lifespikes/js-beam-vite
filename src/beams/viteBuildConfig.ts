4e8414e5904370ceb808809a280f979671d3a01e
CristianHG2 <cherreragiraldo@gmail.com>
2023-04-01 15:29:46 +0000

    prepare release

--- src/beams/viteBuildConfig.ts
+++ src/beams/viteBuildConfig.ts
@@ -1,15 +1,23 @@
 import getHttpConfig from './viteServerConfig';
 import {defineConfig, UserConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import laravel from 'vite-plugin-laravel';
+import laravel from 'laravel-vite-plugin';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import tsconfigPaths from 'vite-tsconfig-paths'
+import * as path from "path";
 
-const viteBuildConfig = (config?: UserConfig) => {
+type LaravelPluginArgsType = Parameters<typeof laravel>[0];
+
+const viteBuildConfig = (config?: UserConfig, laravelConfig?: LaravelPluginArgsType) => {
   return defineConfig({
     ...config,
 
-    plugins: [tsconfigPaths(), cssInjectedByJsPlugin(), react(), laravel(), ...(config?.plugins ?? [])],
+    plugins: [tsconfigPaths({
+      root: path.resolve(process.cwd()),
+    }), cssInjectedByJsPlugin(), react(), laravel(laravelConfig ?? {
+      input: 'resources/js/app.tsx',
+      refresh: true,
+    }), ...(config?.plugins ?? [])],
 
     /* Do not disable. If you're having issues with this contact @CristianHG */
     server: getHttpConfig(),
